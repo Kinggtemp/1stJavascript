5200d1769358fe5e56f2b156a3117e3c2e24a470
Alex Oliver <alex_o1iverz@hotmail.com>
2024-02-03 20:59:09 +0000

    Javascript lesson

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log ('Hello world')
+
+// ctrl / makes selected into a comment
+
+// these are variables
+let firstName = 'Alex';
+let lastName = 'Oliver';
+let age = 21;
+
+//prints to the screen
+console.log(firstName, lastName, age);
+
+/*this is a multi-line comment
+it can span mutilple lines
+and is used for longer comments*/
+
+// this is a single line comment
+
+console.log(20*30);
+// multiply
+console.log(5/2);
+//divide
+console.log(5+5);
+//add
+console.log(5-5);
+//subtraction
+
+console.log('my name is ' + firstName + " " + lastName + ' and i am ' + age + ' years old.')
+
+// modular operator examples
+console.log(5%2);
+//remainder is 1
+console.log(5%3);
+// remainder is 2
+
+    console.log(`my name is ${firstName} ${lastName} and i am ${age} years old.`)
+// easy way, ${variable} with the ` button
+
+
+console.log(age>50);
